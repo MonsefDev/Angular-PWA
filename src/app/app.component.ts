4891342342b2ee357188b8c68064d0da6abe6bb6
LAARAJ <moncif.laaraj@capgemini.com>
2019-07-11 15:19:00 +0100

    my first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService, Item } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'PwaDemo';
+  items:Array<Item>;
+
+  constructor(private apiservice:ApiService){
+
+  }
+
+  ngOnInit(){
+    this.fetchData();
+  }
+  fetchData(){
+    this.apiservice.fetch().subscribe(
+      (data: Array<Item>)=>{
+      console.log(data);
+      this.items=data;
+      },
+      (err)=>{
+      console.log(err);
+    });
+  }
 }
